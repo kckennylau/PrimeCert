79168a69a6512f61f4018f1674311adb9a632d29
Kenny Lau <kc_kennylau@yahoo.com.hk>
2025-10-15 02:22:16 +0100

    powmod by bhavik, prove main pocklington

---
+++ PrimeCert/Pocklington.lean
@@ -0,0 +1,132 @@
+/-
+Copyright (c) 2025 Kenny Lau. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kenny Lau
+-/
+
+import PrimeCert.PowMod
+import Mathlib.Algebra.Field.ZMod
+import Mathlib.Algebra.Order.Ring.Star
+import Mathlib.Analysis.Normed.Ring.Lemmas
+import Mathlib.Data.Int.Star
+import Mathlib.Data.Nat.ChineseRemainder
+import Mathlib.Data.Nat.Totient
+
+/-! # Pocklington's primality certificate
+
+To use this certificate for primality of `N`, factorise `N - 1` completely.
+1. If the largest factor `p` is `> √N`, then set `F₁ = p`.
+2. Otherwise, set `F₁` to be the product of enough prime factors to be `> √N`.
+3. Then, find a primitive root `a` of `N`.
+4. Then `a` will satisfy the conditions required, which are:
+  - `a ^ (N - 1) ≡ 1 (mod N)`
+  - For each prime factor `p` of `F₁`, `gcd(a ^ ((N - 1) / p) - 1, N) = 1`.
+-/
+
+theorem List.pairwise_iff_toFinset {α β : Type*} (l : List α) (hl : l.Nodup)
+    (P : β → β → Prop) (hp : Symmetric P) (e : α → β) [DecidableEq α] :
+    l.Pairwise (P.onFun e) ↔ _root_.Pairwise (P.onFun fun i : l.toFinset ↦ e i) := by
+  rw [Finset.pairwise_subtype_iff_pairwise_finset',
+    List.pairwise_iff_coe_toFinset_pairwise hl (hp.comap _)]
+
+theorem Nat.modEq_finset_prod_iff {a b : ℕ} {ι : Type*} (s : Finset ι) (e : ι → ℕ)
+    (co : Pairwise (Coprime.onFun fun i : s ↦ e i)) :
+    a ≡ b [MOD s.prod e] ↔ ∀ i ∈ s, a ≡ b [MOD e i] := by
+  classical
+  obtain ⟨l, hl, rfl⟩ := s.exists_list_nodup_eq
+  rw [List.prod_toFinset e hl, Nat.modEq_list_map_prod_iff]
+  · simp_rw [List.mem_toFinset]
+  · rwa [List.pairwise_iff_toFinset _ hl _ Coprime.symmetric]
+
+theorem multiplicity_zero_right {α : Type*} [MonoidWithZero α] (x : α) : multiplicity x 0 = 1 :=
+  multiplicity_eq_one_of_not_finiteMultiplicity fun h ↦ h.ne_zero rfl
+
+theorem Nat.modEq_iff_forall_prime_dvd {a b n : ℕ} :
+    a ≡ b [MOD n] ↔ ∀ p : ℕ, p ∣ n → p.Prime → a ≡ b [MOD p ^ multiplicity p n] := by
+  by_cases hn₀ : n = 0
+  · subst hn₀
+    simp_rw [modEq_zero_iff, dvd_zero, true_imp_iff]
+    constructor
+    · rintro rfl; exact fun _ _ ↦ by rfl
+    · intro h
+      obtain ⟨p, hbp, hp⟩ := exists_infinite_primes (a + b + 1)
+      specialize h p hp
+      rw [multiplicity_zero_right, pow_one] at h
+      exact h.eq_of_lt_of_lt (by linarith) (by linarith)
+  · conv_lhs => rw [← factorization_prod_pow_eq_self hn₀]
+    rw [Finsupp.prod, modEq_finset_prod_iff]
+    · simp_rw [support_factorization, mem_primeFactors_of_ne_zero hn₀, and_comm, and_imp]
+      refine forall_congr' fun p ↦ imp_congr_right fun hpn ↦ imp_congr_right fun hp ↦ ?_
+      rw [multiplicity_eq_factorization hp hn₀]
+    · refine fun p q hp ↦ coprime_pow_primes _ _ ?_ ?_ <| by grind
+      · exact ((mem_primeFactors_of_ne_zero hn₀).mp p.2).1
+      · exact ((mem_primeFactors_of_ne_zero hn₀).mp q.2).1
+
+theorem Nat.pow_multiplicity_dvd_of_dvd_of_not_dvd_div
+    {q n x : ℕ} (hq : q.Prime) (hxn : x ∣ n) (hxnq : ¬ x ∣ n / q) :
+    q ^ multiplicity q n ∣ x := by
+  by_cases hqn : q ∣ n
+  · obtain ⟨n, rfl⟩ := hqn
+    rw [Nat.mul_div_cancel_left _ hq.pos] at hxnq
+    by_cases hn₀ : n = 0
+    · subst hn₀; exact (hxnq <| dvd_zero _).elim
+    have hqn₀ : q * n ≠ 0 := mul_ne_zero hq.ne_zero hn₀
+    have hx₀ : x ≠ 0 := by rintro rfl; exact hqn₀ <| zero_dvd_iff.mp hxn
+    rw [← Nat.factorization_le_iff_dvd hx₀ hn₀] at hxnq
+    rw [← Nat.factorization_le_iff_dvd hx₀ hqn₀] at hxn
+    rw [Nat.factorization_mul hq.ne_zero hn₀, hq.factorization, add_comm] at hxn
+    refine pow_dvd_of_le_multiplicity ?_
+    rw [multiplicity_eq_factorization hq hqn₀, multiplicity_eq_factorization hq hx₀,
+      Nat.factorization_mul hq.ne_zero hn₀, Finsupp.add_apply, hq.factorization,
+      Finsupp.single_apply, if_pos rfl, add_comm]
+    refine le_of_not_gt fun h ↦ hxnq fun p ↦ ?_
+    by_cases hpq : p = q
+    · subst hpq; exact lt_succ.mp h
+    convert hxn p using 1
+    rw [Finsupp.add_apply, Finsupp.single_apply, if_neg (Ne.symm hpq), add_zero]
+  · rw [multiplicity_eq_zero.mpr hqn, pow_zero]
+    exact one_dvd _
+
+/-- Let `N` be a natural number whose primality we want to certify.
+Assume we have a partial factorisation `N - 1 = F₁ * R₁`, where `F₁` is fully factorised with
+prime factors `pᵢ`.
+Now for each `pᵢ` find a pseudo-primitive root `aᵢ` such that `aᵢ ^ (N - 1) ≡ 1 (mod N)` and
+`gcd(aᵢ ^ ((N - 1) / pᵢ) - 1, N) = 1`.
+Then any prime factor `n` of `N` satisfies `n ≡ 1 (mod F₁)`. -/
+theorem pocklington_test (N F₁ : ℕ) (hn₀ : 0 < N) (hf₁ : F₁ ∣ N - 1)
+    (primitive : ∀ p ∈ F₁.primeFactors, ∃ a : ℕ, a ^ (N - 1) ≡ 1 [MOD N] ∧
+      Nat.gcd (a ^ ((N - 1) / p) - 1) N = 1)
+    (p : ℕ) (hp : p.Prime) (hpn : p ∣ N) : p ≡ 1 [MOD F₁] := by
+  by_cases hn₁ : N = 1
+  · rw [hn₁, Nat.dvd_one] at hpn
+    exact absurd (hpn ▸ hp) Nat.not_prime_one
+  replace hn₁ : 1 < N := by grind
+  have hf₀ : F₁ ≠ 0 := by
+    rintro rfl
+    rw [zero_dvd_iff] at hf₁
+    grind
+  rw [Nat.modEq_iff_forall_prime_dvd]
+  intro q hqf hq
+  have := Fact.mk hp
+  have := (Nat.prime_iff_card_units _).mp hp
+  rw [Nat.ModEq.comm, Nat.modEq_iff_dvd' hp.one_le, ← this]
+  obtain ⟨a, han, hanq⟩ := primitive q (Nat.mem_primeFactors.mpr ⟨hq, hqf, hf₀⟩)
+  have hanp := han.of_dvd hpn
+  rw [← ZMod.natCast_eq_natCast_iff, Nat.cast_pow, Nat.cast_one] at hanp
+  let a' : (ZMod p)ˣ := Units.ofPowEqOne _ _ hanp (by grind)
+  refine dvd_trans ?_ (orderOf_dvd_card (x := a'))
+  have : multiplicity q F₁ ≤ multiplicity q (N - 1) := by
+    rw [Nat.multiplicity_eq_factorization hq hf₀, Nat.multiplicity_eq_factorization hq (by grind)]
+    exact (Nat.factorization_le_iff_dvd hf₀ (by grind)).mpr hf₁ _
+  refine dvd_trans (pow_dvd_pow _ this) ?_
+  refine Nat.pow_multiplicity_dvd_of_dvd_of_not_dvd_div hq ?_ ?_
+  · rwa [orderOf_dvd_iff_pow_eq_one, Units.ext_iff, Units.val_pow_eq_pow_val]
+  · rw [orderOf_dvd_iff_pow_eq_one, Units.ext_iff, Units.val_pow_eq_pow_val]
+    unfold a'
+    have : 1 ≤ a ^ ((N - 1) / q) := Nat.one_le_pow _ _ <| pos_of_ne_zero fun ha₀ ↦ by
+      subst ha₀; rw [Nat.cast_zero, zero_pow (by grind)] at hanp; simp at hanp
+    rw [Units.val_ofPowEqOne, ← Nat.cast_pow, Units.val_one, ← Nat.cast_one (R := ZMod p),
+      ZMod.natCast_eq_natCast_iff, Nat.ModEq.comm, Nat.modEq_iff_dvd' this,
+      ← hp.coprime_iff_not_dvd]
+    rw [← Nat.coprime_iff_gcd_eq_one] at hanq
+    exact hanq.symm.coprime_dvd_left hpn
---
+++ PrimeCert/PowMod.lean
@@ -0,0 +1,180 @@
+/-
+Copyright (c) 2022 Bhavik Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bhavik Mehta
+-/
+
+import Mathlib.Algebra.Group.Nat.Even
+import Mathlib.Data.Nat.Basic
+import Mathlib.Tactic.NormNum.PowMod
+
+/-!
+# Proof-producing evaluation of `a ^ b % n`
+
+Note that `Mathlib.Tactic.NormNum.PowMod` contains a similar tactic, but that runs significantly
+slower and less efficiently than the one here.
+-/
+
+open Nat
+
+/-- The pow-mod function, named explicitly to allow more precise control of reduction. -/
+def powMod (a b n : ℕ) : ℕ := a ^ b % n
+/-- The pow-mod auxiliary function, named explicitly to allow more precise control of reduction. -/
+def powModAux (a b c n : ℕ) : ℕ := (a ^ b * c) % n
+
+noncomputable def powModTR (a b n : Nat) : Nat :=
+  aux b.succ (a.mod n) b 1
+where
+  aux : Nat → ((a b c : Nat) → Nat) :=
+    Nat.rec (fun _ _ _ => 0)
+      (fun _ r a b c =>
+        (b.beq 0).rec
+          (((b.mod 2).beq 0).rec
+            (r ((a.mul a).mod n) (b.div 2) ((a.mul c).mod n))
+            (r ((a.mul a).mod n) (b.div 2) c))
+          (c.mod n))
+
+def powModTR' (a b n : ℕ) : ℕ :=
+  aux (a % n) b 1
+  where aux (a b c : ℕ) : ℕ :=
+    if b = 0 then c % n
+    else if b = 1 then (a * c) % n
+    else if b % 2 = 0 then
+      aux (a * a % n) (b / 2) c
+    else
+      aux (a * a % n) (b / 2) (a * c % n)
+    partial_fixpoint
+
+lemma Bool.rec_eq_ite {α : Type*} {b : Bool} {t f : α} : b.rec f t = if b then t else f := by
+  cases b <;> simp
+
+@[simp] lemma Nat.mod_eq_mod {a b : ℕ} : a.mod b = a % b := rfl
+@[simp] lemma Nat.div_eq_div {a b : ℕ} : a.div b = a / b := rfl
+@[simp] lemma Nat.land_eq_land {a b : ℕ} : a.land b = a &&& b := rfl
+
+@[simp] lemma powModTR_aux_zero_eq {n a b c : ℕ} :
+    powModTR.aux n 0 a b c = 0 := rfl
+
+lemma powModTR_aux_succ_eq {n a b c fuel : ℕ} :
+    powModTR.aux n (fuel + 1) a b c =
+      (b.beq 0).rec (true := c % n)
+      (((b % 2).beq 0).rec
+        (powModTR.aux n fuel (a * a % n) (b / 2) (a * c % n))
+        (powModTR.aux n fuel (a * a % n) (b / 2) c)) := by
+  rfl
+
+lemma powModTR_aux_succ_eq' {n a b c fuel : ℕ} :
+    powModTR.aux n (fuel + 1) a b c =
+      if b = 0 then c % n else
+      if b % 2 = 0 then powModTR.aux n fuel (a * a % n) (b / 2) c
+      else powModTR.aux n fuel (a * a % n) (b / 2) (a * c % n) := by
+  simp only [powModTR_aux_succ_eq, Bool.rec_eq_ite, beq_eq]
+
+lemma powModTR_aux_eq (n a b c fuel) (hfuel : b < fuel) :
+    powModTR.aux n fuel a b c = powModAux a b c n := by
+  induction fuel generalizing a b c with
+  | zero => omega
+  | succ fuel ih =>
+    rw [powModTR_aux_succ_eq']
+    split
+    case isTrue hb0 => rw [hb0, powModAux, pow_zero, one_mul]
+    split
+    case isTrue hb0 hbe =>
+      rw [ih _ _ _ (by omega)]
+      rw [powModAux, powModAux, Nat.mul_mod _ c, Nat.mul_mod _ c]
+      conv_rhs =>
+        rw [← Nat.mod_add_div b 2]
+      rw [hbe, zero_add, pow_mul, ← pow_two, ← Nat.pow_mod]
+    case isFalse hb0 hbo =>
+      rw [ih _ _ _ (by omega)]
+      rw [powModAux, powModAux, Nat.mul_mod, Nat.mod_mod, ← pow_two,
+        ← Nat.pow_mod, ← Nat.pow_mul, ← Nat.mul_mod, ← mul_assoc, ← Nat.pow_add_one]
+      congr! 3
+      cutsat
+
+lemma powModTR_eq (a b n : ℕ) : powModTR a b n = powMod a b n := by
+  rw [powModTR, powModTR_aux_eq _ _ _ _ _ (by omega)]
+  rw [powModAux, mul_one, powMod, mod_eq_mod, ← Nat.pow_mod]
+
+lemma powMod_eq_of_powModTR (a b n m : ℕ) (h : (powModTR a b n).beq m) : powMod a b n = m := by
+  rwa [powModTR_eq, beq_eq] at h
+
+lemma powMod_ne_of_powModTR (a b n m : ℕ) (h : (powModTR a b n).beq m = false) :
+    powMod a b n ≠ m := by
+  have := Nat.ne_of_beq_eq_false h
+  rwa [powModTR_eq] at this
+
+namespace Tactic.powMod
+
+open Lean Meta Elab Tactic
+
+/-- Given `a, b, n : ℕ`, return `(m, ⊢ powMod a b n = m)`. -/
+def mkPowModEq' (a b n : ℕ) (aE bE nE : Expr) : MetaM (ℕ × Expr × Expr) := do
+  let m := powModTR' a b n
+  let mE := mkNatLit m
+  return (m, mE, mkApp5 (mkConst ``powMod_eq_of_powModTR) aE bE nE mE eagerReflBoolTrue)
+
+/-- Given `a, b, n, m : ℕ`, if `powMod a b n = m` then return a proof of that fact. -/
+def provePowModEq' (a b n m : ℕ) (aE bE nE : Expr) : MetaM Expr := do
+  let (m', _, eq) ← mkPowModEq' a b n aE bE nE
+  unless m = m' do throwError "attempted to prove {a} ^ {b} % {n} = {m} but it's actually {m'}"
+  return eq
+
+/-- Given `a, b, n, m : ℕ`, if `powMod a b n ≠ m` then return a proof of that fact. -/
+def provePowModNe' (a b n m : ℕ) (aE bE nE mE : Expr) : MetaM Expr := do
+  let m' := powModTR' a b n
+  if m = m' then throwError "attempted to prove {a} ^ {b} % {n} ≠ {m} but it is {m'}"
+  return mkApp5 (mkConst ``powMod_ne_of_powModTR) aE bE nE mE eagerReflBoolFalse
+
+def prove_pow_mod_tac (g : MVarId) : MetaM Unit := do
+  let t : Expr ← g.getType
+  match_expr t with
+  | Eq ty lhsE rhsE =>
+    unless (← whnfR ty).isConstOf ``Nat do throwError "not an equality of naturals"
+    let some rhs := rhsE.nat? | throwError "rhs is not a numeral"
+    let some (aE, bE, nE) := lhsE.app3? ``powMod | throwError "lhs is not a pow-mod"
+    let some a := aE.nat? | throwError "base is not a numeral"
+    let some b := bE.nat? | throwError "exponent is not a numeral"
+    let some n := nE.nat? | throwError "modulus is not a numeral"
+    let pf ← provePowModEq' a b n rhs aE bE nE
+    g.assign pf
+  | Ne ty lhsE rhsE =>
+    unless (← whnfR ty).isConstOf ``Nat do throwError "not an equality of naturals"
+    let some rhs := rhsE.nat? | throwError "rhs is not a numeral"
+    let some (aE, bE, nE) := lhsE.app3? ``powMod | throwError "lhs is not a pow-mod"
+    let some a := aE.nat? | throwError "base is not a numeral"
+    let some b := bE.nat? | throwError "exponent is not a numeral"
+    let some n := nE.nat? | throwError "modulus is not a numeral"
+    let pf ← provePowModNe' a b n rhs aE bE nE rhsE
+    g.assign pf
+  | _ => throwError "not an accepted expression"
+
+elab "prove_pow_mod" : tactic => liftMetaFinishingTactic prove_pow_mod_tac
+
+end Tactic.powMod
+
+set_option diagnostics.threshold 0
+set_option diagnostics true
+
+-- theorem powMod_11_100002_100003 : powMod 11 100002 100003 = 1 := by prove_pow_mod
+-- #print powMod_11_100002_100003
+
+-- #time
+-- example :
+--     powMod 2
+--       131715931587485903133664770501783872901180735752961173191222502260846184802138117218820246495979164495762424017769215925882581565859513559697500346853208717730048481311930278737221764046227216650748207028546755348290341925152606053939920784122173626831732721956717186562885471376983969828398653806056
+--       131715931587485903133664770501783872901180735752961173191222502260846184802138117218820246495979164495762424017769215925882581565859513559697500346853208717730048481311930278737221764046227216650748207028546755348290341925152606053939920784122173626831732721956717186562885471376983969828398653806057 =
+--       1 := by
+--   prove_pow_mod
+
+-- #time
+-- example :
+--     powMod 2
+--       131715931587485903133664770501783872901180735752961173191222502260846184802138117218820246495979164495762424017769215925882581565859513559697500346853208717730048481311930278737221764046227216650748207028546755348290341925152606053939920784122173626831732721956717186562885471376983969828398653806056
+--       131715931587485903133664770501783872901180735752961173191222502260846184802138117218820246495979164495762424017769215925882581565859513559697500346853208717730048481311930278737221764046227216650748207028546755348290341925152606053939920784122173626831732721956717186562885471376983969828398653806057 =
+--       1 := by
+--   prove_pow_mod'
+
+-- #eval powModTR' 2 (31 ^ 100) (31 ^ 100 + 7)
+
+-- #eval 68700266508534171304139668405538781983844090880155308447315415736868121904417993012859336490047417444352793669286109720181816751709874535850447534937862879222650085866396891801700313186135778700458822 / 2
