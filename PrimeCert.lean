924f63d9500e53e2dcb0dcbd0579287a8b194395
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-13 17:46:35 +0100

    put the library root on the module system (#143)

--- PrimeCert.lean
+++ PrimeCert.lean
@@ -1,14 +1,16 @@
-import PrimeCert.ForLean
-import PrimeCert.ForMathlib
-import PrimeCert.ForallB
-import PrimeCert.Meta.Pocklington
-import PrimeCert.Meta.Pocklington3
-import PrimeCert.Meta.PowMod
-import PrimeCert.Meta.PrimeCert
-import PrimeCert.Meta.SmallPrime
-import PrimeCert.Pocklington
-import PrimeCert.Pocklington3
-import PrimeCert.PowMod
-import PrimeCert.PredMod
-import PrimeCert.SmallPrimes
-import PrimeCert.Wieferich
+module
+
+public import PrimeCert.ForLean
+public import PrimeCert.ForMathlib
+public import PrimeCert.ForallB
+public import PrimeCert.Meta.Pocklington
+public import PrimeCert.Meta.Pocklington3
+public import PrimeCert.Meta.PowMod
+public import PrimeCert.Meta.PrimeCert
+public import PrimeCert.Meta.SmallPrime
+public import PrimeCert.Pocklington
+public import PrimeCert.Pocklington3
+public import PrimeCert.PowMod
+public import PrimeCert.PredMod
+public import PrimeCert.SmallPrimes
+public import PrimeCert.Wieferich
