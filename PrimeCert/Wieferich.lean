42c1f5ab80b7faaac685e9153515974d78337e3d
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-16 16:21:43 +0100

    Apply suggestion from @b-mehta

--- PrimeCert/Wieferich.lean
+++ PrimeCert/Wieferich.lean
@@ -1,7 +1,9 @@
 /-
 Copyright (c) 2025 Kenny Lau. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Kenny Lau
+Copyright (c) 2025 Kenny Lau, Bhavik Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Kenny Lau, Bhavik Mehta
 -/
 
 module
