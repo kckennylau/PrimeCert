c6050e4e8b6da826469cfe2947c40905e5c548b4
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-16 16:33:36 +0100

    Update copyright notice in Wieferich.lean
    Removed copyright notice for Kenny Lau.

--- PrimeCert/Wieferich.lean
+++ PrimeCert/Wieferich.lean
@@ -1,6 +1,4 @@
 /-
-Copyright (c) 2025 Kenny Lau. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
 Copyright (c) 2025 Kenny Lau, Bhavik Mehta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Kenny Lau, Bhavik Mehta
