4f2531cd45bbcd1407dc0bf7f2ea259f5be8cc00
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-18 00:57:54 +0000

    add cutoffs at 11, 13 and 17

---
+++ PrimeCertTest/WieferichTrial17.lean
@@ -0,0 +1,24 @@
+/-
+Copyright (c) 2026 Bhavik Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bhavik Mehta
+-/
+
+module
+
+import PrimeCertTest.WieferichBenchDefs
+meta import PrimeCert.Meta.QuickRfl
+
+/-! The check skipping any term divisible by a prime below 18. -/
+
+namespace PrimeCert.Bench
+
+set_option maxRecDepth 4000000
+set_option Elab.async false
+
+theorem bench :
+    forallB (fun n ↦ (passes n [5, 7, 11, 13, 17]).not'.or'
+      ((wieferichB n).not'.or' (mirimanoffB n).not')) 1 100000 6 := by
+  quickRfl
+
+end PrimeCert.Bench
