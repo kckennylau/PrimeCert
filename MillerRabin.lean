2869c04c0f9474244a8cda2d601b634a684842f9
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-08-21 20:08:32 +0000

    drop the header from the import-only root

--- MillerRabin.lean
+++ MillerRabin.lean
@@ -1,9 +1,3 @@
-/-
-Copyright (c) 2026 Bhavik Mehta. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Bhavik Mehta
--/
-
 module
 
 public import MillerRabin.Defs
